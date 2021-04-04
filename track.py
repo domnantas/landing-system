36f244fe1add7d63f988cbeb58220e5178be5de5
domnantas <domantas@zyro.com>
2021-04-05 02:37:24 +0300

    Throttle based on target vertical, remove offset

--- track.py
+++ track.py
@@ -29,8 +29,6 @@ class Tracker:
         self.normalized_target_horizontal = None
         self.normalized_target_vertical = None
 
-        self.target_height_offset = -60  # Raise the aiming point
-
         # Subscribe to image topic
         image_topic = "/webcam/image_raw"
         rospy.Subscriber(image_topic, Image, self.image_callback)
@@ -121,19 +119,13 @@ class Tracker:
             self.normalized_target_horizontal = (
                 target_horizontal - frame_width_middle) / frame_width_middle
 
-            if target_vertical <= frame_height_middle + self.target_height_offset:
-                divider = frame_height_middle + self.target_height_offset
-            else:
-                divider = frame_height_middle - self.target_height_offset
-
             self.normalized_target_vertical = (
-                target_vertical - frame_height_middle - self.target_height_offset) / divider
+                target_vertical - frame_height_middle) / frame_height_middle
 
             # Rotate towards target
             roll_limit = 20
-            pitch_limit = 10
-            target_size = cv2.contourArea(largest_contour)
-            throttle = 0.3 if target_size > 400 else 0.5
+            pitch_limit = 5
+            throttle = (-self.normalized_target_vertical + 2.2) * 0.18
 
             frame = cv2.putText(frame, f"Throttle: {throttle}", (10, 80),
                                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
@@ -170,15 +162,11 @@ class Tracker:
         frame_height_middle = int(frame_height / 2)
         crosshair_width = 15
         frame = cv2.line(frame,
-                         (frame_width_middle - crosshair_width,
-                          frame_height_middle + self.target_height_offset),
-                         (frame_width_middle + crosshair_width,
-                          frame_height_middle + self.target_height_offset), (0, 255, 0), 1)
+                         (frame_width_middle - crosshair_width, frame_height_middle),
+                         (frame_width_middle + crosshair_width, frame_height_middle), (0, 255, 0), 1)
         frame = cv2.line(frame,
-                         (frame_width_middle, frame_height_middle -
-                          crosshair_width + self.target_height_offset),
-                         (frame_width_middle, frame_height_middle +
-                          crosshair_width + self.target_height_offset), (0, 255, 0), 1)
+                         (frame_width_middle, frame_height_middle - crosshair_width),
+                         (frame_width_middle, frame_height_middle + crosshair_width), (0, 255, 0), 1)
         return frame
 
     def draw_input(self, frame, normalized_roll=0, normalized_pitch=0):
@@ -188,12 +176,12 @@ class Tracker:
         box_width = 40
         frame = cv2.rectangle(frame,
                               (frame_width_middle - box_width,
-                               frame_height_middle - box_width + self.target_height_offset),
+                               frame_height_middle - box_width),
                               (frame_width_middle + box_width,
-                                  frame_height_middle + box_width + self.target_height_offset), (0, 255, 0), 1)
+                               frame_height_middle + box_width), (0, 255, 0), 1)
         frame = cv2.circle(frame,
-                           (frame_width_middle + int(normalized_roll * box_width), frame_height_middle +
-                            self.target_height_offset - int(normalized_pitch * box_width)), 2, (255, 0, 255), -1)
+                           (frame_width_middle + int(normalized_roll * box_width),
+                            frame_height_middle - int(normalized_pitch * box_width)), 2, (255, 0, 255), -1)
         return frame
 
     def cleanup(self):
