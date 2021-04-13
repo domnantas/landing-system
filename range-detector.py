33a39fced6e911f2c0a872eeb38c13e7ac81cba9
domnantas <domantas@zyro.com>
2021-04-13 19:57:07 +0300

    Refactor range detector

--- range-detector.py
+++ range-detector.py
@@ -31,20 +31,19 @@ def get_arguments():
     ap = argparse.ArgumentParser()
     ap.add_argument('-f', '--filter', required=True,
                     help='Range filter. RGB or HSV')
-    ap.add_argument('-i', '--image', required=False,
-                    help='Path to the image')
-    ap.add_argument('-w', '--webcam', required=False,
-                    help='Use webcam', action='store_true')
-    ap.add_argument('-p', '--preview', required=False,
-                    help='Show a preview of the image after applying the mask',
-                    action='store_true')
+    source = ap.add_mutually_exclusive_group(required=True)
+    source.add_argument('-i', '--image',
+                        help='Path to the image')
+    source.add_argument('-w', '--webcam',
+                        help='Use webcam',
+                        action='store_true')
+    source.add_argument('-s', '--simulator',
+                        help='Use camera input from Gazebo',
+                        action='store_true',)
     args = vars(ap.parse_args())
 
-    if not xor(bool(args['image']), bool(args['webcam'])):
-        ap.error("Please specify only one image source")
-
     if not args['filter'].upper() in ['RGB', 'HSV']:
-        ap.error("Please speciy a correct filter.")
+        ap.error("Filter should be either RGB or HSV.")
 
     return args
 
@@ -60,19 +59,38 @@ def get_trackbar_values(range_filter):
     return values
 
 
+def process_image(image, range_filter):
+    v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(
+        range_filter)
+
+    if range_filter == 'RGB':
+        frame_to_thresh = image.copy()
+    else:
+        frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    thresh = cv2.inRange(
+        frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
+
+    preview = cv2.bitwise_and(image, image, mask=thresh)
+    cv2.imshow("Preview", preview)
+    cv2.imshow("Original", image)
+    cv2.imshow("Thresh", thresh)
+
+    cv2.waitKey(5)
+
+
 def main():
     args = get_arguments()
 
     range_filter = args['filter'].upper()
 
+    setup_trackbars(range_filter)
+
     if args['image']:
         image = cv2.imread(args['image'])
 
-        if range_filter == 'RGB':
-            frame_to_thresh = image.copy()
-        else:
-            frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    else:
+        process_image(image, range_filter)
+    elif args['webcam']:
         from imutils.video.pivideostream import PiVideoStream
         videoStream = PiVideoStream(resolution=(
             640, 480), framerate=20).start()
@@ -86,32 +104,29 @@ def main():
         camera.awb_mode = 'off'
         camera.awb_gains = awb_gains
 
-    setup_trackbars(range_filter)
-
-    while True:
-        if args['webcam']:
+        while True:
             image = videoStream.read()
+            process_image(image, range_filter)
 
-            if range_filter == 'RGB':
-                frame_to_thresh = image.copy()
-            else:
-                frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    elif args['simulator']:
+        from sensor_msgs.msg import Image
+        from cv_bridge import CvBridge
+        import rospy
 
-        v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(
-            range_filter)
+        def ros_image_callback(ros_image, bridge):
+            image = bridge.imgmsg_to_cv2(ros_image, 'bgr8')
+            process_image(image, range_filter)
 
-        thresh = cv2.inRange(
-            frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
+        # Initialize the ros node
+        rospy.init_node('cv_bridge_node', anonymous=True)
+        bridge = CvBridge()
 
-        if args['preview']:
-            preview = cv2.bitwise_and(image, image, mask=thresh)
-            cv2.imshow("Preview", preview)
-        else:
-            cv2.imshow("Original", image)
-            cv2.imshow("Thresh", thresh)
-
-        if cv2.waitKey(1) & 0xFF is ord('q'):
-            break
+        # Subscribe to image topic
+        image_topic = '/webcam/image_raw'
+        rospy.Subscriber(image_topic, Image,
+                         ros_image_callback, callback_args=(bridge))
+        print(f'Subscribed to {image_topic}')
+        rospy.spin()
 
 
 if __name__ == '__main__':
